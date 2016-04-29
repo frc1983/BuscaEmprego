b8359ef887f14d97385da8f0aff53f325d9a199b
Fábio <fabiorochadacosta@gmail.com>
2016-04-28 22:08:58 -0300

    .net version

---
+++ BuscaEmprego/BuscaEmprego/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BuscaEmprego.MvcApplication" Language="C#" %>
---
+++ BuscaEmprego/BuscaEmprego/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuscaEmprego.Startup))]
+namespace BuscaEmprego
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
