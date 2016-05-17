843ca7f7098366aa855215752bb128cab5709178
Fábio <fabiorochadacosta@gmail.com>
2016-05-17 20:48:26 -0300

    Imagens

---
+++ BuscaEmprego/BuscaEmprego/Controllers/VagasController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuscaEmprego.Controllers
+{
+    public class VagasController : Controller
+    {
+        public ActionResult Buscar()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public ActionResult Detalhes()
+        {
+            return View();
+        }
+
+        public ActionResult ListarCandidatos()
+        {
+            return View();
+        }
+    }
+}
